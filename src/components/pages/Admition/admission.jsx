11c37f0310b0fc6601676c679f444046aaea7183
RushikeshGorde <134618502+RushikeshGorde@users.noreply.github.com>
2024-05-13 17:32:26 +0530

    changed

--- src/components/pages/Admition/admission.jsx
+++ src/components/pages/Admition/admission.jsx
@@ -65,13 +65,14 @@ const Admissions = () => {
       ) : (
         <Steps /> // Pass handleGoBack function as a prop
       )}
-      <div className='For_Background_Image'>
+      <div className="imagefix1 text-center">
+  <div className="container">
 
-                <div className="imagefix1"><h1>Tell Us What Are You Looking For</h1>
-                    <h3>It strives to deliver value-based quality education and imbibe top-class management skills</h3>
-                    <button className='contact_us_button'> <a href="/contact-us" target="_self"> CONTACT US </a></button>                </div>
-
-            </div>
+    <h3>Tell Us What Are You Looking For</h3>
+    <h5>It strives to deliver value-based quality education and imbibe top-class management skills</h5>
+    <button className='contact_us_button'> <a href="/contact-us" target="_self"> CONTACT US </a></button>
+  </div>
+</div>
     </main>
   );
 };
--- src/components/pages/school-animities/Aminities.jsx
+++ src/components/pages/school-animities/Aminities.jsx
@@ -109,14 +109,14 @@ const Aminities = () => {
                     <AmenitiesCard key={index} image={amenity.image} heading={amenity.heading} description={amenity.description} />
                 ))}
             </div>
-            <div className='For_Background_Image'>
+            <div className="imagefix1 text-center">
+  <div className="container">
 
-                <div className="imagefix1"><h1>Tell Us What Are You Looking For</h1>
-                    <h3>It strives to deliver value-based quality education and imbibe top-class management skills</h3>
-                    <button className='contact_us_button'> <a href="/contact-us" target="_self"> CONTACT US </a></button>
-                </div>
-
-            </div>
+    <h3>Tell Us What Are You Looking For</h3>
+    <h5>It strives to deliver value-based quality education and imbibe top-class management skills</h5>
+    <button className='contact_us_button'> <a href="/contact-us" target="_self"> CONTACT US </a></button>
+  </div>
+</div>
         </div>
     );
 }
